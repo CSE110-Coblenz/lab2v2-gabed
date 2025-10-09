3c72ab76a37e5c4659cec2af4298af5ca7af163f
EdtheEgghead <edzeegger@gmail.com>
2025-10-09 11:31:05 -0700

    Adds invite list

---
+++ src/guests.ts
@@ -0,0 +1,9 @@
+let people: string[] = [`Lebron`, `Tony Romo`, `John Cena`];
+
+export function printPeople(): void {
+  people.forEach((people, index) => {
+    console.log(`  ${index + 1}. ${snack}`);
+  });
+}
+
+printPeople();
