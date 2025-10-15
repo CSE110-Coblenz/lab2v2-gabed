525142e17d5c6d5ef7812052bb1ff45632f0e020
EdtheEgghead <edzeegger@gmail.com>
2025-10-14 19:04:10 -0700

    Deletes duplicate variable in snacks.ts

--- src/snacks.ts
+++ src/snacks.ts
@@ -1,5 +1,4 @@
 export let snacks: string[] = ["bannana", "cucumbre", "gummies", "cheetohs", "cheese", "shrooms", "cheeze-it"]
-let snacks: string[] = ["bannana", "cucumbre", "gummies", "cheetohs", "potato chips", "chocolate", "candy"]
 
 export function printSnacks(): void {
     snacks.forEach((snack: string) => {
