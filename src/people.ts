00c479ce68f7ae08e4e78f6e83c44614f6679fb8
EdtheEgghead <edzeegger@gmail.com>
2025-10-14 20:09:02 -0700

    Changes people array to cause test to fail

--- src/people.ts
+++ src/people.ts
@@ -1,4 +1,4 @@
-export let people: string[] = ["Lebron", "Bronny", "Savannah"];
+export let people: string[] = ["Lebro", "Bronny", "Savannah"];
 
 export function printPeople(): void {
     people.forEach((snack: string) => {
